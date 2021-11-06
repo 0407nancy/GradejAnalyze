880082eaa8fa4a5bcbefdfb1e096d76c608c16e7
0407nancy <2397028530@qq.com>
2021-11-06 15:31:03 +0800

    a

--- app/src/main/java/com/example/gradeanalyze/GradeAnalyze.java
+++ app/src/main/java/com/example/gradeanalyze/GradeAnalyze.java
@@ -71,85 +71,51 @@ public class GradeAnalyze extends AppCompatActivity {
             int eg2 = Integer.parseInt(gp2str);
             int eg3 = Integer.parseInt(gp3str);
             int eg4 = Integer.parseInt(gp4str);
+
             float weight;//加权平均分—— 综合评级
             weight = (ee1 * eg1 + ee2 * eg2 + ee3 * eg3 + ee4 * eg4) / (eg1 + eg2 + eg3 + eg4);
             BigDecimal turn1 = new BigDecimal(weight);
             float weightFinal = turn1.setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
             wsc.setText(""+weightFinal);//text显示四舍五入、保留两位小数的float变量值
-            if (weight >= 90)
-            {
+            if (weight >= 90) {
                 orkRs.setText("A");
             }
-            else if (weight > 85)
-            {
+            else if (weight > 85) {
                 orkRs.setText("A-");
             }
-            else if (weight > 82)
-            {
+            else if (weight > 82) {
                 orkRs.setText("B+");
             }
-            else if (weight >= 80)
-            {
+            else if (weight >= 80) {
                 orkRs.setText("B");
             }
-            else if (weight >= 76)
-            {
+            else if (weight >= 76) {
                 orkRs.setText("B-");
             }
-            else if (weight >= 73)
-            {
+            else if (weight >= 73) {
                 orkRs.setText("C+");
             }
-            else if (weight >= 70)
-            {
+            else if (weight >= 70) {
                 orkRs.setText("C");
             }
-            else if (weight >= 66)
-            {
+            else if (weight >= 66) {
                 orkRs.setText("C-");
             }
-            else if (weight >= 63)
-            {
+            else if (weight >= 63) {
                 orkRs.setText("D+");
             }
-            else if (weight >= 60)
-            {
+            else if (weight >= 60) {
                 orkRs.setText("D");
             }
-            else
-            {
+            else {
                 orkRs.setText("F");
             }
 
-            float gpa1, gpa2, gpa3, gpa4;//绩点
-            gpa1 = ee1 / 10 - 5;
-            gpa2 = ee2 / 10 - 5;
-            gpa3 = ee3 / 10 - 5;
-            gpa4 = ee4 / 10 - 5;
-        //绩点
-        BigDecimal gpaturn1 = new BigDecimal(gpa1);
-        float gpa1Final = gpaturn1.setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
-        gp1.setText(""+gpa1Final);//四舍五入、保留两位小数
 
-        BigDecimal gpaturn2 = new BigDecimal(gpa2);
-        float gpa2Final = gpaturn2.setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
-        gp2.setText(""+gpa2Final);
-
-        BigDecimal gpaturn3 = new BigDecimal(gpa3);
-        float gpa3Final = gpaturn3.setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
-        gp3.setText(""+gpa3Final);
-
-        BigDecimal gpaturn4 = new BigDecimal(gpa4);
-        float gpa4Final = gpaturn4.setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
-        gp4.setText(""+gpa4Final);
             int tscr;//总学分
             tscr = eg1 + eg2 + eg3 + eg4;
             tsc.setText(""+tscr);
-            float tgpa;//总绩点
-            tgpa = (gpa1 + gpa2 + gpa3 + gpa4) / 4;
-            BigDecimal turn2 = new BigDecimal(tgpa);//四舍五入、保留两位小数
-            float tgpaFinal = turn2.setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
-            tgp.setText(""+tgpaFinal);
+
             float variance;//方差
             variance = (ee1 - weight) * (ee1 - weight) + (ee2 - weight) * (ee2 - weight) + (ee3 - weight) * (ee3 - weight) + (ee4 - weight) * (ee4 - weight);
             if (variance <= 0.99)
@@ -172,186 +138,239 @@ public class GradeAnalyze extends AppCompatActivity {
             if (ee1 >= 90)
             {
                 rt1.setText("A");
+                gp1.setText("4.0");
             }
             else if (ee1 > 85)
             {
                 rt1.setText("A-");
+                gp1.setText("3.7");
             }
             else if (ee1 > 82)
             {
                 rt1.setText("B+");
+                gp1.setText("3.3");
             }
             else if (ee1 >= 80)
             {
                 rt1.setText("B");
+                gp1.setText("3.0");
             }
             else if (ee1 >= 76)
             {
                 rt1.setText("B-");
+                gp1.setText("2.7");
             }
             else if (ee1 >= 73)
             {
                 rt1.setText("C+");
+                gp1.setText("2.3");
             }
             else if (ee1 >= 70)
             {
                 rt1.setText("C");
+                gp1.setText("2.0");
             }
             else if (ee1 >= 66)
             {
                 rt1.setText("C-");
+                gp1.setText("1.7");
             }
             else if (ee1 >= 63)
             {
                 rt1.setText("D+");
+                gp1.setText("1.3");
             }
             else if (ee1 >= 60)
             {
                 rt1.setText("D");
+                gp1.setText("1.0");
             }
             else
             {
                 rt1.setText("F");
+                gp1.setText("0");
             }
 
 
             if (ee2 >= 90)
             {
                 rt2.setText("A");
+                gp2.setText("4.0");
             }
             else if (ee2 > 85)
             {
                 rt2.setText("A-");
+                gp2.setText("3.7");
             }
             else if (ee2 > 82)
             {
                 rt2.setText("B+");
+                gp2.setText("3.3");
             }
             else if (ee2 >= 80)
             {
                 rt2.setText("B");
+                gp2.setText("43.0");
             }
             else if (ee2 >= 76)
             {
                 rt2.setText("B-");
+                gp2.setText("2.7");
             }
             else if (ee2 >= 73)
             {
                 rt2.setText("C+");
+                gp2.setText("2.3");
             }
             else if (ee2 >= 70)
             {
                 rt2.setText("C");
+                gp2.setText("2.0");
             }
             else if (ee2 >= 66)
             {
                 rt2.setText("C-");
+                gp2.setText("1.7");
             }
             else if (ee2 >= 63)
             {
                 rt2.setText("D+");
+                gp2.setText("1.3");
             }
             else if (ee2 >= 60)
             {
                 rt2.setText("D");
+                gp2.setText("1.0");
             }
             else
             {
                 rt2.setText("F");
+                gp2.setText("0");
             }
 
 
             if (ee3 >= 90)
             {
                 rt3.setText("A");
+                gp3.setText("4.0");
             }
             else if (ee3 > 85)
             {
                 rt3.setText("A-");
+                gp3.setText("3.7");
             }
             else if (ee3 > 82)
             {
                 rt3.setText("B+");
+                gp3.setText("3.3");
             }
             else if (ee3 >= 80)
             {
                 rt3.setText("B");
+                gp3.setText("3.0");
             }
             else if (ee3 >= 76)
             {
                 rt3.setText("B-");
+                gp3.setText("2.7");
             }
             else if (ee3 >= 73)
             {
                 rt3.setText("C+");
+                gp3.setText("2.3");
             }
             else if (ee3 >= 70)
             {
                 rt3.setText("C");
+                gp3.setText("2.0");
             }
             else if (ee3 >= 66)
             {
                 rt3.setText("C-");
+                gp3.setText("1.7");
             }
             else if (ee3 >= 63)
             {
                 rt3.setText("D+");
+                gp3.setText("1.3");
             }
             else if (ee3 >= 60)
             {
                 rt3.setText("D");
+                gp3.setText("1.0");
             }
             else
             {
                 rt3.setText("F");
+                gp3.setText("0");
             }
 
 
             if (ee4 >= 90)
             {
                 rt4.setText("A");
+                gp4.setText("4.0");
             }
             else if (ee4 > 85)
             {
                 rt4.setText("A-");
+                gp4.setText("3.7");
             }
             else if (ee4 > 82)
             {
                 rt4.setText("B+");
+                gp4.setText("3.3");
             }
             else if (ee4 >= 80)
             {
                 rt4.setText("B");
+                gp4.setText("3.0");
             }
             else if (ee4 >= 76)
             {
                 rt4.setText("B-");
+                gp4.setText("2.7");
             }
             else if (ee4 >= 73)
             {
                 rt4.setText("C+");
+                gp4.setText("2.3");
             }
             else if (ee4 >= 70)
             {
                 rt4.setText("C");
+                gp4.setText("2.0");
             }
             else if (ee4 >= 66)
             {
                 rt4.setText("C-");
+                gp4.setText("1.7");
             }
             else if (ee4 >= 63)
             {
                 rt4.setText("D+");
+                gp4.setText("1.3");
             }
             else if (ee4 >= 60)
             {
                 rt4.setText("D");
+                gp4.setText("1.0");
             }
             else
             {
                 rt4.setText("F");
+                gp4.setText("0");
             }
-
+//绩点
+        double gpa1 =Double.parseDouble(gp1str);
+        double gpa2 =Double.parseDouble(gp2str);
+        double gpa3 =Double.parseDouble(gp3str);
+        double gpa4 =Double.parseDouble(gp4str);
+        double tgpa;//总绩点
+        tgpa = (gpa1*eg1 + gpa2*eg2 + gpa3*eg3 + gpa4*eg4) / 4;
+        BigDecimal turn2 = new BigDecimal(tgpa);//四舍五入、保留两位小数
+        float tgpaFinal = turn2.setScale(2, BigDecimal.ROUND_HALF_UP).floatValue();
+        tgp.setText(""+tgpaFinal);
         //返回界面
         back1.setOnClickListener(new View.OnClickListener() {
             @Override
